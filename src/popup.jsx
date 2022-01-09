979fad251bea8d373f3ef1b73419e76b8394752e
sid9812 <sidchawla@microsoft.com>
2022-01-09 14:05:15 +0530

    Added basic functionalities

---
+++ src/popup.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import Alert from 'react-bootstrap/Alert'
+import Container from 'react-bootstrap/Container'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const graphql = JSON.stringify({
+    query: "query questionOfToday {  activeDailyCodingChallengeQuestion {  date   userStatus  link   question {     acRate      difficulty     freqBar      frontendQuestionId: questionFrontendId    isFavor     paidOnly: isPaidOnly      status    title    titleSlug     hasVideoSolution      hasSolution      topicTags {       name       id       slug}}}}",
+  })
+
+const alerts = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ].map((variant, idx) => (
+    <Alert key={idx} variant={variant}>
+      This is a {variant} alert with{' '}
+      <Alert.Link href="#">an example link</Alert.Link>. Give it a click if you
+      like.
+    </Alert>
+  ));
+function Popup() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    chrome.cookies.getAll({domain: "leetcode.com", name: "LEETCODE_SESSION"}, (res) => {myHeaders.append("LEETCODE_SESSION", res[0].value)});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: graphql
+        };
+    
+    const [res, setRes] = useState("Loading");
+    const [errorMsg, setErrorMsg] = useState("");
+    const [problem, setProblem] = useState("");
+    const [problemLink, setProblemLink] = useState("");
+
+    useEffect(() => {
+        fetch("https://leetcode.com/graphql", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            setRes(result.data.activeDailyCodingChallengeQuestion.userStatus);
+            setProblem(result.data.activeDailyCodingChallengeQuestion.question.title);
+            setProblemLink("https://leetcode.com"+result.data.activeDailyCodingChallengeQuestion.link);
+            console.log(result);
+        })
+        .catch(error => {
+            setRes("Error");
+            setErrorMsg(error);
+        })
+    }, [])
+
+    chrome.notifications.create('test', {
+        type: 'basic',
+        iconUrl: '/download.png',
+        title: 'Test Message',
+        message: 'You are awesome!',
+        priority: 2
+    });
+    
+    return (
+        
+        <Container>
+            
+            <Alert variant="dark">
+                <center><h1> Leetcode Daily Challenge Reminder! </h1></center>
+            
+
+            {res == "Loading" &&
+                <Container>
+                    <center><h3>Loading...</h3></center>
+                </Container>
+            }
+            
+            {res == "NotStart" &&
+                <Container>
+                    <Alert variant="danger">
+                        <h4>You haven't completed problem for today!</h4>
+                        <h6>Here are the details: </h6>
+                        <p>Today's Problem: &emsp; 
+                            <a href={ problemLink }>{ problem }</a>
+                        </p>
+                        
+                    </Alert>
+                </Container>
+            }
+
+            {res == "Finish" &&
+                <Container>
+                    <Alert variant="success">
+                        <h4>Good Job! You have already completed the question for today!</h4>
+                        <h6>Come back tomorrow for another question! :)</h6>
+                    </Alert>
+                </Container>
+            }
+
+            {res == "Error" && 
+                <Container>
+
+                </Container>
+            }
+            </Alert>
+        </Container>
+    );
+}
+
+render(<Popup />, document.getElementById("react-target"));
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        popup: '.\/src\/popup.jsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                }
+            }
+        },
+        {
+            test: /\.(sass|css|scss)$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              {
+                loader: "postcss-loader",
+                options: {
+                  plugins: () => [
+                    require("autoprefixer")()
+                  ],
+                },
+              },
+              'sass-loader',
+            ]
+        },
+    ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: '.\/src\/popup.html',
+        filename: 'popup.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: "public" },
+            ],
+        }),
+    ],
+};
